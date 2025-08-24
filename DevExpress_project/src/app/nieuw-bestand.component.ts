6b9335b84b669ba8e8f29376d46b86f438a6e70a
Kruijsbergen <cornel.kruijsbergen@arcadis.com>
2025-08-24 03:58:34 +0200

    Alternatieve upload functionaliteit

--- DevExpress_project/src/app/nieuw-bestand.component.ts
+++ DevExpress_project/src/app/nieuw-bestand.component.ts
@@ -1,20 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DxSelectBoxModule } from 'devextreme-angular/ui/select-box';
+import { DxNumberBoxModule } from 'devextreme-angular/ui/number-box';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+
+interface Onderdeel {
+  OnderdeelID: string;
+  Nominaal_Vermogen: number;
+  Piek_Vermogen: number;
+  Gewicht: number;
+  Levensduur: number;
+  Capaciteit: number;
+  Categorie: string;
+  kwantiteit?: number;
+}
 
 @Component({
   selector: 'app-nieuw-bestand',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DxSelectBoxModule, DxNumberBoxModule],
   templateUrl: './nieuw-bestand.component.html',
   styleUrls: ['./nieuw-bestand.component.css']
 })
 export class NieuwBestandComponent {
+  // --- Oude upload functionaliteit ---
   geselecteerdBestand: File | null = null;
   uploadResult: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {
+    this.laadCategorieen();
+  }
 
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
@@ -49,5 +66,44 @@ export class NieuwBestandComponent {
 
   gaNaarPVSystemen() {
     this.router.navigate(['/pvsystemen']);
+  }
+
+  // --- Nieuwe onderdelenlijst functionaliteit ---
+  categorieen: string[] = [];
+  onderdelen: Onderdeel[] = [];
+  geselecteerdeCategorie: string | null = null;
+  geselecteerdOnderdeel: Onderdeel | null = null;
+  kwantiteit: number = 1;
+  lijst: Onderdeel[] = [];
+
+  laadCategorieen() {
+    this.http.get<string[]>('/api/categorieen').subscribe(data => {
+      this.categorieen = data;
+    });
+  }
+
+  laadOnderdelen() {
+    if (!this.geselecteerdeCategorie) return;
+    this.http.get<Onderdeel[]>(`/api/onderdelen?categorie=${this.geselecteerdeCategorie}`).subscribe(data => {
+      this.onderdelen = data;
+    });
+  }
+
+  voegToeAanLijst() {
+    if (this.geselecteerdOnderdeel && this.kwantiteit > 0) {
+      this.lijst.push({ ...this.geselecteerdOnderdeel, kwantiteit: this.kwantiteit });
+      this.geselecteerdOnderdeel = null;
+      this.kwantiteit = 1;
+    }
+  }
+
+  verwijderUitLijst(index: number) {
+    this.lijst.splice(index, 1);
+  }
+
+  verderNaarPVSystemen() {
+    this.http.post('/api/verwerk_onderdelen', { onderdelen: this.lijst }).subscribe(() => {
+      this.router.navigate(['/pvsystemen']);
+    });
   }
 }
